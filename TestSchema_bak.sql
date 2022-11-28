4f62143caa810218d2363d91f955df67d5e6615b
yaayaya <44697709+yaayaya@users.noreply.github.com>
2022-11-28 16:12:33 +0800

    create shcema

--- TestSchema_bak.sql
+++ TestSchema_bak.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `testschema` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `testschema`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: testschema
@@ -49,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-28 15:25:37
+-- Dump completed on 2022-11-28 16:12:04
